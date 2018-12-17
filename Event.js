d44fc09a97b96560ce7bdc8de0d5640e9cdd62c4
nicolas.guitard <guitardnicolas@hotmail.fr>
2018-12-17 12:22:59 +0100

    creation class event et listEvent

---
+++ Event.js
@@ -0,0 +1,11 @@
+class Event {
+
+    constructor(idEvent, title, dateDebut, dateFin, description, idUser) {
+        this.idEvent = idEvent;
+        this.title = title;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.description = description;
+        this.idUser = idUser;
+    }
+}
