75ae5d33c98445ddbce236b28061de9606aa3172
Maël Baron <mael.baron@laposte.net>
2018-12-17 11:38:23 +0100

    maj index.js
    ajout route login

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000 // this is very important
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({ extended: false})
+const tabUser = [
+    {mail: 'roger@gmail.com', password: 'toto' },
+    {mail: 'nico@gmail.com', password: 'titi'}
+];
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey = 'monSecret';
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+passport.use(new JwtStrategy(opts, function(jwt_payload, done){
+    console.log(jwt_payload);
+    done(null, 'test')
+}));
+
+app.get('/public', (req, res) => {
+    res.send('I am public folks!')
 })
 
-app.get('/private', function (req, res) {
-    res.send('Hello World Private !')
+app.get('/private', passport.authenticate('jwt', { session: false }),(req, res) => {
+    res.send('Hello' + req.user.email)
 })
 
-app.get('/public', function (req, res) {
-    res.send('Hello World Public')
-})
+app.post('/login/', urlEncodedParser,(req, res) => {
+    const email = req.body.mail;
+    const password = req.body.password;
 
-app.post('/login', (req, res) => {
+    if (!email || !password) {
+        res.status(401).json({error: 'Le mail ou le mdp est introuvable.'})
+        return
+    }
+
+    const user = tabUser.find(user => user.mail === email)
+
+    if (!user || user.password !== password) {
+        res.status(401).json({error: 'Le mot de passe est incorrect}'})
+        return
+    }
+
+    const userjwt = jwt.sign({user : email}, 'monSecret')
+
     res.json({
-        jwt: '' // TODO
+
+        jwt: userjwt
     })
 })
 
-app.listen(PORT, function () {
-    console.log('Example app listening on port ' + PORT)
+app.listen(3000, () => {
+    console.log('app running on port 3000')
 })
