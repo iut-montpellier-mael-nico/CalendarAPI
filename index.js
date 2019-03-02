3971380c0a121b2de9be8d7265cd8df9a769194e
Maël Baron <mael.baron@laposte.net>
2019-03-02 16:12:47 +0100

    edit caractere route "" => ''
    pb avce route get resolu

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 const bodyParser = require("body-parser");
-const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
 var user = require('./User');
 var event = require('./Event');
-const PORT = process.env.PORT ||5000
-const monUser = new user();
+const PORT = process.env.PORT || 5000
 
 
 const tabUser = [];
@@ -17,77 +16,79 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const passport = require("passport");
 
 const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "monSecret"
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: "monSecret"
 };
 
 passport.use(
-  new JwtStrategy(opts, function(jwt_payload, done) {
-    console.log(jwt_payload);
-    done(null, "test");
-  })
+    new JwtStrategy(opts, function (jwt_payload, done) {
+        console.log(jwt_payload);
+        done(null, "test");
+    })
 );
 
-app.get("/public", (req, res) => {
-  res.send("afficher Calendrier");
+app.get('/public', (req, res) => {
+    res.send("afficher Calendrier");
 });
 
-app.get("/private",passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.send("Hello" + req.user.email +" partie pour les personnes connecter");
-  }
+app.get('/private', passport.authenticate("jwt", {session: false}),
+    (req, res) => {
+        res.send("Hello" + req.user.email + " partie pour les personnes connecter");
+    }
 );
 
-app.get("/private/mesEvents",passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.send(monUser.getListEvent);
-  }
+app.get('/private/mesEvents', passport.authenticate("jwt", {session: false}),
+    (req, res) => {
+        res.send(monUser.getListEvent);
+    }
 );
 
-app.get("/private/addEvent",passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const idEvent = req.body.idEvent;
-    const title = req.body.title; 
-    const dateDebut = req.body.dateDebut;
-    const dateFin = req.body.dateFin;
-    const description = req.body.dateFin; 
-    const idUser = req.body.idUser;
-    event1 = new event(idEvent,title,dateDebut,dateFin,description,idUser);
-    monUser.addEvent(event1);
-  }
+app.get('/private/addEvent', passport.authenticate("jwt", {session: false}),
+    (req, res) => {
+        const idEvent = req.body.idEvent;
+        const title = req.body.title;
+        const dateDebut = req.body.dateDebut;
+        const dateFin = req.body.dateFin;
+        const description = req.body.dateFin;
+        const idUser = req.body.idUser;
+        event1 = new event(idEvent, title, dateDebut, dateFin, description, idUser);
+        monUser.addEvent(event1);
+    }
 );
 
-app.post("/login", urlEncodedParser, (req, res) => {
-  const email = req.body.mail;
-  const password = req.body.password;
+app.post('/login', urlEncodedParser, (req, res) => {
+    const email = req.body.mail;
+    const password = req.body.password;
 
-  if (!email || !password) {
-    res.status(401).json({ error: "Le mail ou le mdp est introuvable." });
-    return;
-  }
+    if (!email || !password) {
+        res.status(401).json({error: "Le mail ou le mdp est introuvable."});
+        return;
+    }
 
-  const user = tabUser.find(user => user.mail === email);
+    const user = tabUser.find(user => user.mail === email);
 
-  if (!user || user.password !== password) {
-    res.status(401).json({ error: "Le mot de passe est incorrect" });
-    return;
-  }
+    if (!user || user.password !== password) {
+        res.status(401).json({error: "Le mot de passe est incorrect"});
+        return;
+    }
 
-  const userjwt = jwt.sign({ user: email }, "monSecret");
+    const userjwt = jwt.sign({user: email}, "monSecret");
 
-  res.json({
-    jwt: userjwt
-  });
+    res.json({
+        jwt: userjwt
+    });
 });
 
-app.post("/register",(req, res) => {
-monUser = new user(req.body.mail, req.body.password)
-tabUser.push(monUser);
+app.post('/register', (req, res) => {
+    let monUser = new user(req.body.mail, req.body.password)
+    tabUser.push(monUser);
 });
 
-app.get("/", (req,res) {
+app.get('/', function (req, res)
+{
     res.send('Hello World je suis une appli qui fonctionne !')
-})
+}
+)
 app.listen(PORT, () => {
-  console.log("app running on port " + PORT);
+    console.log("app running on port " + PORT);
 });
